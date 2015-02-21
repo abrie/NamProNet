0bb79d7675f65717b159c148b4547e16091d050a
abrie <abrie@>
2015-02-21 14:01:21 +0000

    remove build and add signup

--- phonegap/platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Users/abrie/development/npn/npn_webservice/phonegap/platforms/android/ant-build/CordovaApp-debug-unaligned.apk : \
-/Users/abrie/development/npn/npn_webservice/phonegap/platforms/android/ant-build/CordovaApp.ap_ \
-/Users/abrie/development/npn/npn_webservice/phonegap/platforms/android/ant-build/classes.dex \
-/Users/abrie/development/npn/npn_webservice/phonegap/platforms/android/CordovaLib/ant-build/classes.jar \
--- phonegap/platforms/android/ant-build/classes.dex.d
+++
@@ -1,9 +0,0 @@
-/Users/abrie/development/npn/npn_webservice/phonegap/platforms/android/ant-build/classes.dex : \
-/Users/abrie/development/npn/npn_webservice/phonegap/platforms/android/ant-build/classes/na/com/npn/BuildConfig.class \
-/Users/abrie/development/npn/npn_webservice/phonegap/platforms/android/ant-build/classes/na/com/npn/CordovaApp.class \
-/Users/abrie/development/npn/npn_webservice/phonegap/platforms/android/ant-build/classes/na/com/npn/R$attr.class \
-/Users/abrie/development/npn/npn_webservice/phonegap/platforms/android/ant-build/classes/na/com/npn/R$drawable.class \
-/Users/abrie/development/npn/npn_webservice/phonegap/platforms/android/ant-build/classes/na/com/npn/R$string.class \
-/Users/abrie/development/npn/npn_webservice/phonegap/platforms/android/ant-build/classes/na/com/npn/R$xml.class \
-/Users/abrie/development/npn/npn_webservice/phonegap/platforms/android/ant-build/classes/na/com/npn/R.class \
-/Users/abrie/development/npn/npn_webservice/phonegap/platforms/android/ant-build/dexedLibs/classes-2b18ead8a94073f7d2738c3203b343ee.jar \
