5efc848f7ae4e946c31fcacc7b0ec5c9ba309225
abrie <abrie@>
2015-02-21 11:56:50 +0000

    add Profile model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def after_database_authentication
+      profile = Profile.find_by_user_id(self.id)
+      if profile.nil? then
+          Profile.create
+      end
+
+  end
 end
